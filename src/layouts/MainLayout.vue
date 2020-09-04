fb35c63830bf7f91ff34dd5dba5bc56750f02fc2
jisbruzzi <jose.sbru@gmail.com>
2020-09-04 07:41:01 -0300

    e390 - Add FileStructure component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,5 @@
 <template>
   <q-layout view="hHh Lpr fFf">
-    <q-header>
-      <q-toolbar>
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
     <q-page-container>
       <q-splitter
         v-model="splitterModel"
@@ -17,8 +7,7 @@
       >
         <template v-slot:before>
           <div class="q-pa-md">
-            <div class="text-h4 q-mb-md">Before</div>
-            <div v-for="n in 20" :key="n" class="q-my-md">{{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</div>
+            <file-structure/>
           </div>
         </template>
 
@@ -33,11 +22,12 @@
 
 <script lang="ts">
 import EssentialLink from 'components/EssentialLink.vue'
+import FileStructure from 'components/FileStructure.vue'
 
 import { Vue, Component } from 'vue-property-decorator'
 
 @Component({
-  components: { EssentialLink }
+  components: { EssentialLink, FileStructure }
 })
 export default class MainLayout extends Vue {
   splitterModel = 20;
