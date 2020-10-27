c8046f06b21a4317aa694e5260b2c29c959b420c
jisbruzzi <jose.sbru@gmail.com>
2020-10-27 15:29:38 -0300

    b695 - custom label for Lines of Code

--- src/components/Utils.ts
+++ src/components/Utils.ts
@@ -1,11 +1,16 @@
-export function currentMetricName (visualization:Record<string, any>):string {
+export function currentMetricName (visualization: Record<string, any>): string {
   return `${normalCasing(visualization.parameters.metric)} (${visualization.parameters.scaling} scaling)`;
 }
 
-export function descriptionOfVisualization (visualization:Record<string, any>):string {
+export function descriptionOfVisualization (visualization: Record<string, any>): string {
   return `<strong>${normalCasing(visualization.visualizationType)}</strong> of ${visualization.parameters.scaling} <strong>${normalCasing(visualization.parameters.metric)}</strong> on ${visualization.parameters.community}`;
 }
 
-export function normalCasing (camelCase:string):string {
-  return camelCase.replace(/([a-z0-9])([A-Z])/g, '$1 $2').toLowerCase()
+export function normalCasing (camelCase: string): string {
+  if (camelCase.toLowerCase() === 'linesofcode') {
+    return 'LOC (Lines of Code)'
+  } else {
+    return camelCase.replace(/([a-z0-9])([A-Z])/g, '$1 $2').toLowerCase()
+  }
+
 }
