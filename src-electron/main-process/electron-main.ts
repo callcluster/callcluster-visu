89e23af3b6b4fd1e5286a866e5a77a47b846a779
jisbruzzi <jose.sbru@gmail.com>
2020-11-16 07:51:16 -0300

    aced - rename files

--- src-electron/main-process/electron-main.ts
+++ src-electron/main-process/electron-main.ts
@@ -1,6 +1,6 @@
 import { app, BrowserWindow, nativeTheme, Menu, MenuItem, dialog, ipcMain } from 'electron'
 import fs from 'fs'
-import { setAnalysisJson, getAvailableMetrics, makeVisualization, getInfoFor } from './logic'
+import { setAnalysisJson, getAvailableMetrics, makeVisualization, getInfoFor } from './callcluster-visu-logic'
 try {
   if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
     require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
