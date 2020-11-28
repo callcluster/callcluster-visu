a9542c15822ecc04b12d59cec2e684689fc8a951
jisbruzzi <jose.sbru@gmail.com>
2020-11-28 20:42:44 -0300

    2ff2 - fixed bins for histogram

--- src-electron/main-process/callcluster-visu-logic/makeVisualization/histogram/makeHistogram.ts
+++ src-electron/main-process/callcluster-visu-logic/makeVisualization/histogram/makeHistogram.ts
@@ -1,32 +1,46 @@
 import HistogramVisualization from "./HistogramVisualization";
 import Analyzable from "./_Analyzable";
-import makeEvaluator from "./_makeEvaluator";
-export default function makeHistogram(visualization:HistogramVisualization, analyzable:Analyzable) {
-    let { bins = 100 } = visualization.parameters;
-    const evaluate = makeEvaluator(visualization, analyzable)
+import makeEvaluator, { SubjectEvaluator } from "./_makeEvaluator";
+import {Function} from './_types'
 
+function calculateBins(limits:{min:number,max:number},preferredBins:number|undefined):{binSize:number,bins:number} {
+    if(preferredBins===undefined){
+        preferredBins=100
+    }
+    const {min,max}=limits
+    const idealBinSize = (max - min) / preferredBins
+    const binSize = [1,2,5,10,50,100].find(s => s>idealBinSize) ?? idealBinSize
+    return {
+        binSize,
+        bins:Math.ceil(Math.ceil(max - min)/binSize)
+    }
+}
+
+function getLimits(functions: Function[], evaluate: SubjectEvaluator):{min:number,max:number}{
     let min = Infinity
     let max = -Infinity
-    for (const func of analyzable.getWrittenFunctions()) {
+    for (const func of functions) {
         const val = evaluate(func)
-        if (val!==undefined && val < 100000) {
+        if (val!==undefined && val>0) {
             min = Math.min(val, min)
             max = Math.max(val, max)
         }
     }
-    let binSize = (max - min) / bins
-    if (binSize < 1) {
-        binSize = 1
-        bins = Math.ceil(max - min)
-    }
+    return {min, max}
+}
+export default function makeHistogram(visualization:HistogramVisualization, analyzable:Analyzable) {
+    const evaluate = makeEvaluator(visualization, analyzable)
+    const {min, max} = getLimits(analyzable.getWrittenFunctions(), evaluate)
+
+    const {binSize,bins} = calculateBins({min,max},visualization.parameters.bins)
     const histogram = new Array(bins).fill(0).map((v, i) => ({
         y: 0,
-        min: (min + binSize * i),
-        max: (min + binSize * (i + 1))
+        min: (min + binSize * i - 1),
+        max: (min + binSize * (i + 1) - 1)
     }))
     for (const func of analyzable.getWrittenFunctions()) {
         const x = evaluate(func)
-        if( x === undefined ){
+        if( x === undefined || x <= 0){
             continue;
         }
         const bin = Math.floor((x - min) / binSize)
